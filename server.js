f0bbe668f48d720f352c9c70c1841d166f7008cb
vladiepops <vladislav.malai-cic@ibm.com>
2022-06-29 20:23:28 +0200

    simple connection between frontend-backend

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require('http')
 const app = require('./backend/app')
+const request = require('request')
 
+
+const url = 'https://api.openbrewerydb.org/breweries/'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
 const server = http.createServer(app)
+
+request({ url: url + 'MadTree Brewing' }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data)
+})
 
 server.listen(port)
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  searchInput = ''
+  searchResult = ''
 
-  constructor() { }
+  onSearch() {
+    const searchResult = {
+      content: this.searchInput
+    }
+    console.log(searchResult)
+    this.http.post<{message: string}>("http://localhost:3000/api/beers", searchResult).subscribe((responseData) => {
+      console.log(responseData.message)
+    })
+    // this.http.get('http://localhost:3000/api/beers')
+    // .subscribe((beerData) => {})
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
