0ff25b4bab1f9d73929bbdadab584a4089e5e3fe
Rongrong Xu <rongrong.xu-cic@ibm.com>
2022-06-30 17:06:26 +0200

    before pull new code

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
 
