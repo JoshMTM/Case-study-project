f7c3b3e4703e673e6b664611dbc0324a1a04812d
Anatolii Zadvornyi <anatolii.zadvornyi-cic@ibm.com>
2022-06-30 14:44:52 +0200

    fixed connection to the backend issue

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader(
         "Access-Control-Allow-Headers", 
-        "Origin, X-Requested-With, Content-Type, Accept")
+        "Origin, X-Requested-With, Content-Type, Accept, x-rapidapi-key, x-rapidapi-host")
     res.setHeader(
         "Access-Control-Allow-Methods", 
         "GET, POST, PATCH, DELETE, OPTIONS")
